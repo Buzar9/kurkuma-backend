6657cca1a408556e691abc1f63eea3d51786b144
Buzar9 <48588984+Buzar9@users.noreply.github.com>
2020-04-01 15:52:07 +0200

    Create Quests Entity

---
+++ src/main/java/pl/zhr/scouting/entity/Quests.java
@@ -0,0 +1,51 @@
+package pl.zhr.scouting.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "quests")
+public class Quests {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "quests_id")
+    private int questsId;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "job")
+    private String job;
+
+    public Quests() {
+    }
+
+    public Quests(String description, String job) {
+        this.description = description;
+        this.job = job;
+    }
+
+    public int getQuestsId() {
+        return questsId;
+    }
+
+    public void setQuestsId(int questsId) {
+        this.questsId = questsId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+}
