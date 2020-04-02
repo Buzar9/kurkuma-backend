34eee35044fc233a618e9dae46fe7ba68f24190c
Buzar9 <48588984+Buzar9@users.noreply.github.com>
2020-04-02 17:55:07 +0200

    Get List in QuestController

--- src/main/java/pl/zhr/scouting/controller/QuestController.java
+++ src/main/java/pl/zhr/scouting/controller/QuestController.java
@@ -1,4 +1,24 @@
 package pl.zhr.scouting.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.zhr.scouting.entity.Quest;
+import pl.zhr.scouting.repository.QuestRepository;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "*")
 public class QuestController {
+
+    @Autowired
+    private QuestRepository questRepositoryImpl;
+
+    @GetMapping("/")
+    public List<Quest> questList(){
+
+        return questRepositoryImpl.findAll();
+    }
 }
