68dac703535a270116f71589a0da57ec77a06743
Buzar9 <48588984+Buzar9@users.noreply.github.com>
2020-04-02 17:56:53 +0200

    Get List in RealizationController

--- src/main/java/pl/zhr/scouting/controller/RealizationController.java
+++ src/main/java/pl/zhr/scouting/controller/RealizationController.java
@@ -1,4 +1,24 @@
 package pl.zhr.scouting.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.zhr.scouting.entity.Realization;
+import pl.zhr.scouting.repository.RealizationRepository;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "*")
 public class RealizationController {
+
+    @Autowired
+    private RealizationRepository realizationRepositoryImpl;
+
+    @GetMapping("/realizations")
+    public List<Realization> realizationList(){
+
+        return realizationRepositoryImpl.findAll();
+    }
 }
