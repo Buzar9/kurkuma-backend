8f5f197ac956e560c127aba17173c02ed8c8c691
Buzar9 <48588984+Buzar9@users.noreply.github.com>
2020-04-04 09:23:40 +0200

    Update QuestController.java

--- src/main/java/pl/zhr/scouting/controller/QuestController.java
+++ src/main/java/pl/zhr/scouting/controller/QuestController.java
@@ -21,11 +21,31 @@ public class QuestController {
         return questRepositoryImpl.findAll();
     }
 
+    @GetMapping("{questId}")
+    public Quest findById(@PathVariable int questId) {
+
+        return questRepositoryImpl.findById(questId);
+    }
+
     @PostMapping("/achievement{achievementId}")
     public void saveQuest(@PathVariable int achievementId,
                           @RequestBody Quest tempQuest){
 
         tempQuest.setQuestId(0);
-        questRepositoryImpl.saveOrUpdate(tempQuest, achievementId);
+        questRepositoryImpl.save(tempQuest, achievementId);
+    }
+
+    @PutMapping("{questId}")
+    public void updateQuest(@PathVariable int questId,
+                            @RequestBody Quest tempQuest) {
+
+        tempQuest.setQuestId(questId);
+        questRepositoryImpl.update(tempQuest);
+    }
+
+    @DeleteMapping("{questId}")
+    public void deleteQuest(@PathVariable int questId) {
+
+        questRepositoryImpl.delete(questId);
     }
 }
